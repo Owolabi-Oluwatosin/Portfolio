f1179658e344ead1c74303d0d118bf0f591f628c
Oluwatosin Daniel Owolabi <danielty2k@gmail.com>
2024-01-09 11:35:19 +0100

    change sanity plugin

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,17 +1,16 @@
-import {defineConfig} from 'sanity/lib/exports'
+import {defineConfig, isDev} from 'sanity/lib/exports'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
+const devOnlyPlugins = [deskTool(), visionTool()]
+
 export default defineConfig({
   name: 'default',
   title: 'portfolio',
-
   projectId: 'q5dx793t',
   dataset: 'production',
-
-  plugins: [deskTool(), visionTool()],
-
+  plugins: [...(isDev ? devOnlyPlugins : [])],
   schema: {
     types: schemaTypes,
   },
